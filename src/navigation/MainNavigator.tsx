251370c72e560b62a26f7122d1e8662181fd9409
Jorge Daniel Sosa <JDSDigital@users.noreply.github.com>
2022-08-17 15:29:36 +0100

    Feat: Adds logos and splash (#17)

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,5 +1,4 @@
 import { Account } from "screens/Main/Account"
-import Colors from "../constants/Colors"
 import { FontAwesome5 } from "@expo/vector-icons"
 import { Home } from "screens/Main/Home"
 import { MainTabParamList } from "../models/Navigation"
@@ -8,17 +7,19 @@ import { Routes } from "models/Routes"
 import { Wallet } from "screens/Main/Wallet"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import useColorScheme from "../hooks/useColorScheme"
+import { useTheme } from "native-base"
 
 const BottomTab = createBottomTabNavigator<MainTabParamList>()
 
 export function MainNavigator() {
   const colorScheme = useColorScheme()
+  const { colors } = useTheme()
 
   return (
     <BottomTab.Navigator
       initialRouteName={Routes.main.home}
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
+        tabBarActiveTintColor: colorScheme === "dark" ? colors.white : colors.black,
         headerShown: false,
       }}
     >
