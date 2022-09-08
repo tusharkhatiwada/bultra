8caa83f944946f5a4671d44b7bfd567dd630053e
Carlos Díaz <linkmetal18@gmail.com>
2022-09-08 15:49:08 +0100

    fixed statusbar background (#57)

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import "./src/config/i18n"
 import "./src/config/yupLocale"
 
+import { LogBox, SafeAreaView, View } from "react-native"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import { ApiProvider } from "context/ApiContext"
 import { AuthProvider } from "context/AuthContext"
-import { LogBox } from "react-native"
 import { NativeBaseProvider } from "native-base"
 import Navigation from "./src/navigation/NavigationContainer"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { StatusBar } from "expo-status-bar"
 import { ToastProvider } from "context/ToastContext"
+import { colors } from "styles/colors"
 import { createTheme } from "styles/theme"
 import useCachedResources from "./src/hooks/useCachedResources"
 import useColorScheme from "./src/hooks/useColorScheme"
@@ -29,6 +30,8 @@ export default function App() {
 
   LogBox.ignoreLogs(["i18next::pluralResolver"])
 
+  const backgroundColor = colorScheme === "dark" ? colors.black : colors.white
+
   if (!isLoadingComplete) {
     return null
   } else {
@@ -39,8 +42,16 @@ export default function App() {
             <SafeAreaProvider>
               <NativeBaseProvider theme={createTheme(colorScheme)}>
                 <ToastProvider>
+                  <View style={{ backgroundColor }}>
+                    <SafeAreaView>
+                      <StatusBar
+                        translucent
+                        backgroundColor={backgroundColor}
+                        style={colorScheme === "dark" ? "light" : "dark"}
+                      />
+                    </SafeAreaView>
+                  </View>
                   <Navigation colorScheme={colorScheme} />
-                  <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
                 </ToastProvider>
               </NativeBaseProvider>
             </SafeAreaProvider>
