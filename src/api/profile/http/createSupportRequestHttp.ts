3ff4e24b4f2a3afff62d09c9b22ddca24429bc29
Jorge Daniel Sosa <JDSDigital@users.noreply.github.com>
2022-09-19 11:35:05 +0100

    Fix: Rename const and urls (#68)

--- src/api/profile/http/createSupportRequestHttp.ts
+++ src/api/profile/http/createSupportRequestHttp.ts
@@ -2,11 +2,12 @@ import { AxiosInstance, AxiosResponse } from "axios"
 
 import { SupportRequest } from "api/domain/profile"
 
-export const createChangePasswordHttp =
+export const createSupportRequestHttp =
   (client: AxiosInstance): SupportRequest.Request =>
-  async ({ phoneNumber }) => {
+  async ({ phoneNumber, message }) => {
     const { data }: AxiosResponse<string> = await client.post("/v1/profile/support-request", {
       phoneNumber,
+      message,
     })
 
     return data
--- src/api/wallet/http/createWithdrawalRequestHttp.ts
+++ src/api/wallet/http/createWithdrawalRequestHttp.ts
@@ -5,7 +5,7 @@ import { WithdrawalRequest } from "api/domain/wallet"
 export const createWithdrawalRequestHttp =
   (client: AxiosInstance): WithdrawalRequest.Request =>
   async ({ amount, network, walletAddress }) => {
-    const { data }: AxiosResponse<string> = await client.post("/v1/profile/support-request", {
+    const { data }: AxiosResponse<string> = await client.post("/v1/wallet/withdraw", {
       amount,
       network,
       walletAddress,
