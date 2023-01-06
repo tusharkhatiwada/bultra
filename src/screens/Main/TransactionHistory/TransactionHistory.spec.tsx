46f33a3af95b773d3e0a6e3bd3ce49fe16bf72cd
Danil Kondratov <com.kondratov@gmail.com>
2023-01-06 07:40:59 +0100

    fix TransactionHistory test

--- src/screens/Main/TransactionHistory/TransactionHistory.spec.tsx
+++ src/screens/Main/TransactionHistory/TransactionHistory.spec.tsx
@@ -1,18 +1,14 @@
 import { TransactionHistory, TransactionHistoryProps } from "./TransactionHistory"
 import { render, waitFor } from "tests/app-tests-utils"
 
-const props = {
-  navigation: {
-    navigate: jest.fn(),
-  },
-} as unknown as TransactionHistoryProps
+const props = {} as TransactionHistoryProps
 
 describe("TransactionHistory", () => {
   it("displays the default message", async () => {
     const { getByText } = await render(<TransactionHistory {...props} />)
 
     await waitFor(() => {
-      expect(getByText("history.title")).toBeTruthy()
+      expect(getByText("wallet.history.title")).toBeTruthy()
     })
   })
 })
