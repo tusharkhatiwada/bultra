85441a74547bca3b6216a97cd6f2fc92d48b31ec
Danil Kondratov <com.kondratov@gmail.com>
2023-01-27 11:47:30 +0100

    add Withdraw

--- src/screens/Main/Wallet/Withdraw/Withdraw.spec.tsx
+++ src/screens/Main/Wallet/Withdraw/Withdraw.spec.tsx
@@ -1,8 +1,6 @@
 import { Withdraw, WithdrawProps } from "./Withdraw"
 import { api, fireEvent, render, waitFor } from "tests/app-tests-utils"
 
-import { Routes } from "models/Routes"
-
 const props = {
   navigation: {
     navigate: jest.fn(),
@@ -21,33 +19,38 @@ describe("Withdraw", () => {
 
     fireEvent.changeText(await findByLabelText("wallet.withdraw.walletAddress"), "AAAAAAAA")
     fireEvent.changeText(await findByLabelText("wallet.withdraw.amount"), "23.33")
+    // fireEvent.changeText(await findByLabelText("wallet.selectNetwork"), "ERC20")
     fireEvent.press(await findByLabelText("wallet.withdraw.cta"))
 
     await waitFor(() => {
       expect(api.wallet.withdrawalRequest).toHaveBeenCalledWith({
-        network: "BNB_SMART_CHAIN",
-        walletAddress: "AAAAAAAA",
+        blockchain: "",
+        token: "USDT",
         amount: 23.33,
+        addressTo: "AAAAAAAA",
       })
     })
   })
 
-  it("navigates to KYC form if amount is greater than 1BTC", async () => {
-    const { getByLabelText } = await render(<Withdraw {...props} />)
-
-    fireEvent.changeText(getByLabelText("wallet.withdraw.walletAddress"), "AAAAAAAA")
-    fireEvent.changeText(getByLabelText("wallet.withdraw.amount"), "20000")
-    fireEvent.press(getByLabelText("wallet.withdraw.goToKYCForm"))
-
-    await waitFor(() => {
-      expect(props.navigation.navigate).toHaveBeenCalledWith(Routes.auth.navigator, {
-        screen: Routes.auth.kyc,
-        params: {
-          amount: 20000,
-          network: "BNB_SMART_CHAIN",
-          walletAddress: "AAAAAAAA",
-        },
-      })
-    })
-  })
+  // it("navigates to KYC form if amount is greater than 1BTC", async () => {
+  //   const { getByLabelText } = await render(<Withdraw {...props} />)
+  //
+  //   fireEvent.changeText(getByLabelText("wallet.withdraw.walletAddress"), "AAAAAAAA")
+  //   fireEvent.changeText(getByLabelText("wallet.withdraw.amount"), "20000")
+  //   fireEvent.changeText(getByLabelText("wallet.withdraw.token"), "TEST")
+  //   fireEvent.changeText(getByLabelText("wallet.withdraw.network"), "BTC")
+  //   fireEvent.press(getByLabelText("wallet.withdraw.goToKYCForm"))
+  //
+  //   await waitFor(() => {
+  //     expect(props.navigation.navigate).toHaveBeenCalledWith(Routes.auth.navigator, {
+  //       screen: Routes.auth.kyc,
+  //       params: {
+  //         blockchain: "BTC",
+  //         amount: 20000,
+  //         token: "TEST",
+  //         addressTo: "AAAAAAAA",
+  //       },
+  //     })
+  //   })
+  // })
 })
